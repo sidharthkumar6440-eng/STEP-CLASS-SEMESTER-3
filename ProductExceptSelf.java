ee4c71116c0e06e6bf154455214ee277f819fd2a
Sidharth Kumar <sidharthkumar6440@gmail.com>
2026-09-10 23:55:26 +0530

    Add files via upload

---
+++ ProductExceptSelf.java
@@ -0,0 +1,50 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ProductExceptSelf {
+
+    static int[] productExceptSelf(int[] nums) {
+
+        int n = nums.length;
+        int[] answer = new int[n];
+
+        // Left product
+        int leftProduct = 1;
+
+        for (int i = 0; i < n; i++) {
+            answer[i] = leftProduct;
+            leftProduct = leftProduct * nums[i];
+        }
+
+        // Right product
+        int rightProduct = 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            answer[i] = answer[i] * rightProduct;
+            rightProduct = rightProduct * nums[i];
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int[] result = productExceptSelf(nums);
+
+        System.out.println("Output: " + Arrays.toString(result));
+
+        sc.close();
+    }
+}
