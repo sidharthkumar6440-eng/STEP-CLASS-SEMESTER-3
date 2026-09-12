29505b0b582e6a3eea377dda80508c7577f27618
Sidharth Kumar <sidharthkumar6440@gmail.com>
2026-09-12 16:23:15 +0530

    Add files via upload

---
+++ M3_Course.java
@@ -0,0 +1,33 @@
+// M3. Course Credit Management
+class Course {
+    String code;
+    String title;
+    int credits;
+    int labCredits;
+
+    public Course(String code, String title, int credits, int labCredits) {
+        this.code = code;
+        this.title = title;
+        this.credits = credits;
+        this.labCredits = labCredits;
+    }
+
+    // Theory-only constructor, chains to the 4-arg constructor
+    public Course(String code, String title, int credits) {
+        this(code, title, credits, 0);
+    }
+
+    int totalCredits() {
+        return credits + labCredits;
+    }
+}
+
+public class M3_Course {
+    public static void main(String[] args) {
+        Course theoryCourse = new Course("21CSC201J", "Data Structures", 4);
+        Course labCourse = new Course("21CSC205L", "DSA Lab", 3, 1);
+
+        System.out.println(theoryCourse.code + " total credits: " + theoryCourse.totalCredits());
+        System.out.println(labCourse.code + " total credits: " + labCourse.totalCredits());
+    }
+}
